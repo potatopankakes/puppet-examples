4846a1d99b76ab058ecb3ab97018d77ff9a24396
Jeff Burke <potatopankakes@gmail.com>
2016-02-05 01:22:51 -0700

    add documentation for the example

--- puppet/manifests/example1/node.pp
+++ puppet/manifests/example1/node.pp
@@ -1,18 +1,35 @@
+#
+# example1 illustrates the need to reverse the order of the relationships when one
+# wishes to uninstall (ensure==absent) components
+#
+# usage:
+#  mkdir vm
+#  cp Vagrantfile.example1 vm/
+#  cd vm
+#  vagrant up
+#  vagrant ssh
+#  sudo puppet apply --parser future --debug --modulepath=/puppet/modules /puppet/manifests/example1/
+#
 node example1 {
-    $ensure = absent
-    example1::service1 { 's1':
-      ensure => $ensure,
-    }
-    example1::files1 { 'f1':
-      ensure => $ensure,
-    }
-    example1::systemd_reload { 'systemd_reload': }
 
-    if $ensure == present {
-      Example1::Files1['f1'] -> Example1::Systemd_reload['systemd_reload'] -> Example1::Service1['s1']
-    }
-    else {
-      Example1::Service1['s1'] -> Example1::Systemd_reload['systemd_reload'] -> Example1::Files1['f1']
-    }
-    #Example1::Files1['f1'] ~> Example1::Service1['s1']
+  # toggle these back and forth to examine the different sequence in the puppet logs
+  #$ensure = absent
+  $ensure = present
+
+  # define the 3 resources to illustrate the install/uninstall sequences
+  example1::service1 { 's1':
+    ensure => $ensure,
+  }
+  example1::files1 { 'f1':
+    ensure => $ensure,
+  }
+  example1::systemd_reload { 'systemd_reload': }
+
+  if $ensure == present {
+    # comment out the encasing if-else block, and only keep this line to see the incorrect sequencing if the relationships are not reversed
+    Example1::Files1['f1'] -> Example1::Systemd_reload['systemd_reload'] -> Example1::Service1['s1']
+  }
+  else {
+    Example1::Service1['s1'] -> Example1::Systemd_reload['systemd_reload'] -> Example1::Files1['f1']
+  }
 }
--- puppet/modules/example1/manifests/init.pp
+++ puppet/modules/example1/manifests/init.pp
@@ -1,9 +1,14 @@
+#
+# service definition
+#
 define example1::service1 (
   $ensure
 ) {
+  # these two logging statements illustrate that the resource is executed twice... and yet
+  # only on the 2nd iteration are the changes 'run/applied'
   debug( "--- in service1: ${ensure}, runs ONCE" )
   notice( "--- in service1: ${ensure}, runs TWICE" )
-  #notify { "---  -- in service1: ${ensure}, runs ONCE": }
+
   $service_enable = $ensure ? {
     absent  => false,
     default => true,
@@ -19,18 +24,26 @@ define example1::service1 (
   }
 }
 
+#
+# systemd unit file
+#
 define example1::files1 (
   $ensure
 ) {
+# these two logging statements illustrate that the resource is executed twice... and yet
+# only on the 2nd iteration are the changes 'run/applied'
   debug( "--- in files1: ${ensure}, runs ONCE" )
   notice( "--- in files1: ${ensure}, runs TWICE" )
-  #notify{ "--- -- in files1: ${ensure}, runs ONCE": }
+
   file { '/usr/lib/systemd/system/svc1.service' :
     ensure => $ensure,
     content => "[Unit]\nDescription=svc1 service 2\n\n[Service]\nType=simple\nExecStart=/usr/bin/sh -c 'while true; do sleep 1; done;'\nKillMode=process\n\n[Install]\nWantedBy=multi-user.target\n",
   }
 }
 
+#
+# reloads Unit file changes into systemd
+#
 define example1::systemd_reload {
   exec { 'systemctl-daemon-reload' :
     command => '/usr/bin/systemctl daemon-reload'
